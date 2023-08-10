eb78f0f54eb9f4b053d58c2b8f01fe13c04582ba
JustinsCOC <138391907+JustinsCOC@users.noreply.github.com>
2023-08-09 17:33:21 -0700

    Update main.py
    Adjusted the code to account for zero stars and not to award the dip / hitting up star adjustments

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import openpyxl
 
 # Load the overall sheet containing all attacker names
-workbook = openpyxl.load_workbook('Put the location of your sheet')
+workbook = openpyxl.load_workbook('route to the location of sheet')
 overall_sheet = workbook.worksheets[0]
 
 # Create a dictionary to store calculated data for each attacker
 attacker_data = {}
 
-# Create a dictionary to store total offense scores for each attacker
-attacker_offense = {}
-
-# Create a dictionary to store net stars for each attacker
-attacker_net_stars = {}
-
 # Iterate through each round sheet (2 to 8)
 for round_number in range(1, 8):
     round_sheet = workbook.worksheets[round_number]
+
+    # Create a dictionary to store total offense scores for each attacker in this round
+    attacker_offense_round = {}
+
+    # Create a dictionary to store scores for each attacker in this round
+    attacker_score_round = {}
 
     # Iterate through each row in the round sheet
     for row in round_sheet.iter_rows(min_row=2, values_only=True):
@@ -31,22 +31,40 @@ for round_number in range(1, 8):
         attacker_townhall = float(attacker_townhall) if attacker_townhall else 0
         defender_townhall = float(defender_townhall) if defender_townhall else 0
         defender_stars = float(defender_stars) if defender_stars else 2
-        attacker_offense_score = float(attacker_offense_score) if attacker_offense_score else 1
+        attacker_offense_score = float(attacker_offense_score) if attacker_offense_score else 0
 
-        # Calculate the difference and update the attacker's data
-        diff = (defender_townhall - attacker_townhall) - defender_stars
-        if attacker_name in attacker_data:
-            attacker_data[attacker_name].append(diff)
-            attacker_offense[attacker_name] += attacker_offense_score
+        # Calculate total offense and update the attacker's data for this round
+        if attacker_name in attacker_offense_round:
+            attacker_offense_round[attacker_name] += attacker_offense_score
         else:
-            attacker_data[attacker_name] = [diff]
-            attacker_offense[attacker_name] = attacker_offense_score
+            attacker_offense_round[attacker_name] = attacker_offense_score
 
-# Calculate net stars and store them in the attacker_net_stars dictionary
-for attacker_name in attacker_data:
-    total_value = sum(attacker_data[attacker_name])
-    total_offense = attacker_offense[attacker_name]
-    net_stars = total_value + total_offense
+        # Calculate score and update the attacker's data for this round
+        if attacker_offense_score == 0:
+            score = -defender_stars
+        else:
+            score = (defender_townhall - attacker_townhall) - defender_stars
+        if attacker_name in attacker_score_round:
+            attacker_score_round[attacker_name].append(score)
+        else:
+            attacker_score_round[attacker_name] = [score]
+
+    # Update the attacker_data dictionary with total offense and scores for this round
+    for attacker_name, offense_score in attacker_offense_round.items():
+        if attacker_name in attacker_data:
+            attacker_data[attacker_name]['offense'].append(offense_score)
+            if attacker_name in attacker_score_round:
+                attacker_data[attacker_name]['scores'].append(attacker_score_round[attacker_name])
+        else:
+            attacker_data[attacker_name] = {'offense': [offense_score],
+                                            'scores': [attacker_score_round.get(attacker_name, [])]}
+
+# Calculate the net stars for each player and store them in the attacker_net_stars dictionary
+attacker_net_stars = {}
+for attacker_name, data in attacker_data.items():
+    total_scores = sum(sum(round_scores) for round_scores in data['scores'])
+    total_offense = sum(data['offense'])
+    net_stars = total_scores + total_offense
     attacker_net_stars[attacker_name] = net_stars
 
 # Sort the attacker_net_stars dictionary by values (net stars)
